e49fc48c68ef11520822da925e726b0d13848bfe
PhilSciMath <leandro.ff7v2@gmail.com>
2025-01-15 20:09:17 -0300

    Finished chapter 8, functions

---
+++ python_crash_course_exercises/functions.py
@@ -0,0 +1,7 @@
+""" Contains some functions required for some of the exercises """
+
+def car_info_builder(manufacturer, model_name, **car_info):
+    """ Builds a dictionary with info about a car """
+    car_info['manufacturer'] = manufacturer
+    car_info['model_name'] = model_name
+    return car_info
